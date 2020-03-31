a8b68c69eac1095e5972a80adf7348acd8404564
Zerotopia <guillaume.cano42@hotmail.fr>
2020-03-31 14:07:58 +0200

    Authentication by google and facebook

--- app/src/main/java/com/example/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_KEY = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startSignInActivity();
+    }
+
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build()
+                        ))
+                        .setTheme(R.style.SignInTheme)
+                        .setLogo(R.drawable.ic_graphic_3433081)
+                        .build(), SIGN_IN_KEY);
     }
 }
