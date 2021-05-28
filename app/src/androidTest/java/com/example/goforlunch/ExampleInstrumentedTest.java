89437ce1975149f7343f7607039b0815310d2b14
Cano <guillaume.cano42@hotmail.fr>
2021-05-28 17:57:36 +0200

    fix package name in test

--- app/src/androidTest/java/com/example/goforlunch/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/goforlunch/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.go4lunch", appContext.getPackageName());
+        assertEquals("com.example.goforlunch", appContext.getPackageName());
     }
 }
